9954a68c7d90720df90d62b03b9736e79ddeece2
MsManish09 <msmanish680@gmail.com>
2025-07-16 17:24:14 +0530

    js lecture 5 : for of , for in

--- lecture5.js
+++ lecture5.js
@@ -1,8 +1,26 @@
    
+let arr = [1, 2 ,3, 5, 7, 10]
 
 
+let obj = {
+    a: 10,
+    b: "hello", 
+    c: true,
+    d_fn : function(){
+        console.log( "this is method, fucntions inside a object  (obj))" )
+    }
+}
 
+console.log(obj.d_fn())
+obj.d_fn()
 
+for (let elements of arr){
+    console.log(elements)
+} // for (in) used to iterate arrays
 
+// for (of) used to iterate objects
 
-   
+for (let keys in obj){
+    console.log(keys)
+    console.log(obj[keys])
+} // for (in) used to iterate over objects
