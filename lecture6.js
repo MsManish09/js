fc595da1214233f8ae1ae07c501d960a562a8b88
MsManish09 <msmanish680@gmail.com>
2025-07-21 14:34:40 +0530

    array methods

--- lecture6.js
+++ lecture6.js
@@ -16,13 +16,15 @@ var a = 10
 
 // 1. fn(a) taking another fn(b) as args. fn(a) is HOF
 
+// HOF Function....
 function o(x){
     console.log('this is function o')
     x() // This is function p
 }
-
+ 
+// Call back function...
 function p(){
-    console.log('this is function p')
+    console.log('this is function p') 
 }
 
 o(p) // function o is HOF.
@@ -44,4 +46,22 @@ console.log(u)  // ƒ h(){
 //         console.log('this is funciton h')
 //     }   -------------> fn g is hof
 
+// -------------------------------------------------------
+
+
+// Array Methods
+
+let arr = [10, 20, 30, 40]
+
+b = arr.push(25) // returns arr lenght.
+console.log(arr, b) // arr and arr 
+
+let r = arr.pop() // removes and return a element from the end
+console.log(r) // 25
+
+b = arr.unshift(70, 60 , 80) // adds new elements at the start
+console.log(arr, b) // [70, 60, 80, 10, 20, 30, 40],  length - 7
+
+r = arr.shift() // remove 1st element for a array and  returns it
+console.log(arr, r) // [60, 80, 10, 20, 30, 40], removed element - 70
 
