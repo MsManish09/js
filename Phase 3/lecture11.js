6676845450a2dce05bf61fa4cfa938c74fa48be2
MsManish09 <msmanish680@gmail.com>
2025-07-26 18:07:42 +0530

    settimeout example

--- Phase 3/lecture11.js
+++ Phase 3/lecture11.js
@@ -7,10 +7,25 @@ console.log('study end')
 
 // Asynchronous: 
 
-console.log('study start') 
+// console.log('study start') 
 
-// want to delay execution delay for 2sec
-setTimeout( () => {console.log("study for 2 sec")}, 3000 )
-// exection is delayed 2secs
+// // want to delay execution delay for 2sec
+// setTimeout( () => {console.log("study for 2 sec")}, 3000 )
+// // exection is delayed 2secs
 
-console.log('study end')
+// console.log('study end')
+
+// --------------------------------
+
+console.log('manish')
+
+setTimeout(() => {
+    console.log("hwllo after 4sec")
+},4000);
+
+setTimeout(() => {
+    console.log("hwllo after 2sec")
+},2000);
+
+
+console.log('Bye')
