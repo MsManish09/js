a55e5442c58e1be67ae52ab027a53e0b17ea38da
MsManish09 <msmanish680@gmail.com>
2025-07-26 19:13:37 +0530

    Instagram Task

--- Phase 3/lecture11.js
+++ Phase 3/lecture11.js
@@ -5,7 +5,7 @@
 // console.log('studing for 1 min')
 // console.log('study end')
 
-// Asynchronous: 
+// Asynchronous: (settimeout())
 
 // console.log('study start') 
 
@@ -31,10 +31,65 @@
 
 //  timer after '0'
 
-console.log('hello')
+// console.log('hello')
 
-setTimeout(() => {
-    console.log("hwllo after 0sec")
-},0);
+// setTimeout(() => {
+//     console.log("hwllo after 0sec")
+// },0);
 
-console.log('bye')
+// console.log('bye')
+
+
+console.log("-------------------------------")
+
+
+// Instagram task using settimeout()
+
+
+console.time("myFunctionExecution");
+    
+//step1:  Selecting image 4s
+
+function SelectingImg(){
+    console.log('select image')
+
+    setTimeout(() => {
+        console.log('image selected');
+        filter("PIC1")
+    }, 4000);
+}
+
+// Apply filter 2s
+function filter(image){
+    console.log(`adding filter for ${image}`)
+
+    setTimeout(() => {
+        console.log('Filter Added');
+        caption(image, "cool filter")
+    }, 2000);
+}
+
+// Apply Caption 5s
+function caption(image, filter){
+    console.log(`adding caption for ${image} with ${filter}`)
+
+     setTimeout(() => {
+        console.log('cation Added');
+        Uploading(image, filter, 'cool caption')
+    }, 5000);
+}
+
+// Uploading img 4s
+function Uploading(img, filter, caption){
+    console.log(`uploading ${img} with ${filter} with ${caption}`)
+
+    setTimeout(() => {
+        console.log( `${img} uploaded with ${filter} and this ${caption}`);
+
+        console.timeEnd("myFunctionExecution"); // code to check amount of time taken to run the instagram task.
+    }, 4000);
+}
+
+SelectingImg()
+
+
