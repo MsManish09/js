1eb38d31690a7c0e114a2c297abe1afcfc4b22c4
MsManish09 <msmanish680@gmail.com>
2025-07-29 20:55:21 +0530

    api calling using fetch() and axios()

---
+++ Phase 3/lecture13_api_calling.js
@@ -0,0 +1,56 @@
+
+// fetch using promise
+
+// let api = 'https://jsonplaceholder.typicode.com/todos/1'
+
+// fetch(api)// return a promise
+// .then((resp)=>{
+//     return resp.json() // json() return a promise
+// })
+// .then((resp) =>{
+//     console.log(resp)
+// })
+// .catch((e)=>{console.log('somethingwent wrong', e)})
+
+// console.log('------------------------------------------------------------------------------')
+
+
+
+// // Fetch and async - await
+
+// let api1 = 'https://jsonplaceholder.typicode.com/todos/1'
+
+// async function apiFetch() {
+//     const rsp = await fetch(api1)
+//     const finRsp = await rsp.json()
+
+//     console.log(finRsp)
+//     console.log('------------------------------------------')
+// }
+
+// apiFetch()
+
+// ------------------------------------------
+
+// Axios()
+
+// using promise
+
+let api = "https://jsonplaceholder.typicode.com/todos/1"
+
+// axios.get(api)
+// .then((value) =>{
+//     console.log("Value: ", value.data)
+// })
+// .catch((err) =>{
+//     console.log(err)
+// })
+
+// using async - await
+
+async function calling() {
+    const resp = await axios.get(api)
+    console.log(resp.data)
+}
+
+calling()
