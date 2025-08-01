d3afd7517d4766ad316ca4c2834cabeaaa7a5a5d
MsManish09 <msmanish680@gmail.com>
2025-08-01 17:48:48 +0530

    DOM Tree

--- Phase 3/lecture14_DOM.js
+++ Phase 3/lecture14_DOM.js
@@ -1,4 +1,6 @@
 
+/*
+
 let h1 = document.querySelector('h1')
 // console.log(h1)
 
@@ -17,3 +19,55 @@ console.log(h1.classList.contains('txt_dec')) // to check if the class is added 
 
 h1.classList.toggle('txt_dec') // In this case, Adds.
 console.log(h1.classList.contains('txt_dec')) // True
+
+*/
+
+// --------------------DOM Tree ------------
+
+let h1 = document.querySelector('h1')
+// console.log(h1)
+
+h1.setAttribute('class', 'headers txt_dec') // is not present, add the class. if present removes the class in this case REMOVES
+// h1.classList.add('headers')
+
+let p = document.querySelectorAll('p')
+
+for(let paras of p ){
+    paras.classList.toggle('para')
+}
+
+let ol = document.querySelector('ol')
+    ol.classList.add('list')
+    ol.classList.add('txt_dec')
+
+let listItems = document.querySelectorAll('li')
+
+for(let items of listItems){
+    items.classList.toggle('items')
+}
+
+console.log(ol)
+console.log(ol.parentElement)
+
+let listItem_1 = document.querySelector('#list1')
+console.log(listItem_1)
+
+console.log(listItem_1.parentElement) //ol#orderedlist.list.txt_dec
+
+console.log(listItem_1.parentElement.childElementCount) //4
+
+console.log(listItem_1.parentElement.children[0]) //li#list1.items
+console.log(listItem_1.parentElement.children[1]) // //li#lis2.items
+console.log(listItem_1.parentElement.children[2]) // //li#list3.items
+console.log(listItem_1.parentElement.children[3]) // //li#list4.items
+
+
+console.log(listItem_1.nextElementSibling) // li#list2.items
+
+console.log(listItem_1.nextElementSibling.nextElementSibling) // li#list3.items
+
+console.log(listItem_1.parentElement.parentElement) // section#2
+
+listItem_1.nextElementSibling.classList.toggle('selectedItems') // item 2 , class changes
+
+listItem_1.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle('selectedItems') // item 4, class changed
